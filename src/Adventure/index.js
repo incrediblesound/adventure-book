304ac5a984169cd3116d52455c8cf248a3273089
James Edwards <james.howe.edwards@gmail.com>
2018-12-28 14:15:43 -0800

    Update story parser and add exclude

--- src/Adventure/index.js
+++ src/Adventure/index.js
@@ -197,6 +197,12 @@ class App extends Component {
             <p>{`${option.text} (use ${option.lock})`}</p>
           </Choice>
         )
+      } else if (option.exclude && !gameState.playerHasHiddenItem(option.exclude)){
+        return (
+          <Choice onClick={() => this.handleGo(option.target)}>
+          <p>{`${option.text}`}</p>
+          </Choice>
+        )
       } else if (!option.condition || (option.condition && gameState.playerHasHiddenItem(option.condition))) {
         return (
           <Choice onClick={() => this.handleGo(option.target)}>
